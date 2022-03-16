05387fb18e3f2937c448e1b5c2af3aa44ebbcf1f
devpoi <smboy86@naver.com>
2022-03-16 23:56:28 +0900

    dev - 이것저것..

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,40 @@
-import { Text } from 'react-native-elements';
-import { Box } from '../components/basic';
+import { Feather, Fontisto } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Button, Text } from 'native-base';
+import { useEffect, useLayoutEffect } from 'react';
+import { Alert } from 'react-native';
+import { Box, BoxPressable } from '../components/basic';
+import { MainDrawStackNavigateProps } from '../types';
 
-import { RootLoginStackScreenProps } from '../types';
+export default function HomeScreen() {
+  const navigation = useNavigation<MainDrawStackNavigateProps<'Home'>>();
 
-export default function HomeScreen({
-  navigation,
-}: RootLoginStackScreenProps<'Login'>) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: (props) => (
+        <BoxPressable onPress={() => Alert.alert('', '새필름 만들기')} pr={12}>
+          <Feather name='film' size={24} color={props.tintColor} />
+        </BoxPressable>
+      ),
+    });
+  }, [navigation]);
+
+  // const route = useRoute<MainDrawScreenProps<'History'>>();
+
+  // useEffect(() => {
+  // }, []);
+
   return (
-    <Box full center>
-      <Text>홈</Text>
+    <Box full ph={16}>
+      <Box wFull row aCenter pv={30}>
+        <Text fontSize={'3xl'} mr={2}>
+          List
+        </Text>
+        <Fontisto name='film' size={30} />
+      </Box>
+      <Box>
+        <Text fontSize={18}>필름이름</Text>
+      </Box>
     </Box>
   );
 }
